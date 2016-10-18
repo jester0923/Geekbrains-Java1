095accc25ab20ed786e2b44050d31f976a88285f
jester0923 <jester2309@gmail.com>
2016-10-18 21:41:52 +0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+* Created by Dmtriy Kirpichnikov 05.10.2016
+*/
+
+public class HelloWorld {
+	    public static void main (String[] args) 
+		{
+			System.out.println ("Hello World!");
+		} 
+}
+
+	
